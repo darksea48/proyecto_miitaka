59f4a5c98f0e47e8cab7c32f163ecb009e7253e2
Douglas Suárez Zamorano <d.suarez.zamorano@gmail.com>
2025-11-23 19:37:16 -0300

    correcciones en settings.py

--- Proy_Itaka/settings.py
+++ Proy_Itaka/settings.py
@@ -92,7 +92,7 @@ WSGI_APPLICATION = 'Proy_Itaka.wsgi.application'
 DATABASES = {
     'default': dj_database_url.config(
         default=os.environ.get('DATABASE_URL'),
-        conn_max_age=600,
+        conn_max_age=600
     )
 }
 
